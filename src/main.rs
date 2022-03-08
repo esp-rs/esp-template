ecabe0b65ea899ed28489f6187db0f5553b0caf1
Jesse Braham <jesse@beta7.io>
2022-03-08 10:27:45 -0800

    Add the first (quite rough) version of the template

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+#![no_std]
+#![no_main]
+
+use {{ mcu }}_hal::{pac::Peripherals, prelude::*};
+use panic_halt as _;
+{% if mcu == "esp32c3" %}
+use riscv_rt::entry;
+{% else %}
+use xtensa_lx_rt::entry;
+{% endif %}
+
+#[entry]
+fn main() -> ! {
+    let peripherals = Peripherals::take().unwrap();
+
+    loop {}
+}
