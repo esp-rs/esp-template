248095908c136d61845863ed209fd8ac5328fead
Sergio Gasquez <sergio.gasquez@gmail.com>
2023-01-13 12:28:21 +0100

    style: 🎨 Format

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,49 @@
 #![no_std]
 #![no_main]
 
-{% if alloc -%}
+{%- if alloc -%}
 extern crate alloc;
-{% endif -%}
+{%- endif %}
 
 use {{ mcu }}_hal::{clock::ClockControl, pac::Peripherals, prelude::*, timer::TimerGroup, Rtc};
-use esp_backtrace as _;{% if mcu == "esp32s2" %}
-use xtensa_atomic_emulation_trap as _;{% endif %}
-{% if alloc %}#[global_allocator]
+use esp_backtrace as _;
+{% if mcu == "esp32s2" -%}
+use xtensa_atomic_emulation_trap as _;
+{% endif %}
+
+{% if alloc %}
+#[global_allocator]
 static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
 
 fn init_heap() {
     const HEAP_SIZE: usize = 32 * 1024;
 
     extern "C" {
-        static mut _heap_start: u32;{% if mcu != "esp32c3" %}
-        static mut _heap_end: u32;{% endif %}
+        static mut _heap_start: u32;
+        {% if mcu != "esp32c3" -%}
+        static mut _heap_end: u32;
+        {% endif %}
     }
 
     unsafe {
-        let heap_start = &_heap_start as *const _ as usize;{% if mcu != "esp32c3" %}
+        let heap_start = &_heap_start as *const _ as usize;
+        {% if mcu != "esp32c3" -%}
         let heap_end = &_heap_end as *const _ as usize;
         assert!(
             heap_end - heap_start > HEAP_SIZE,
             "Not enough available heap memory."
-        );{% endif %}
+        );
+        {% endif %}
         ALLOCATOR.init(heap_start as *mut u8, HEAP_SIZE);
     }
-}{% endif %}
-{% if mcu == "esp32c3" -%}
+}
+{% endif %}
+{%- if mcu == "esp32c3" -%}
 #[riscv_rt::entry]
-{% else -%}
+{%- else -%}
 #[xtensa_lx_rt::entry]
-{% endif %}fn main() -> ! {
+{% endif %}
+fn main() -> ! {
     let peripherals = Peripherals::take().unwrap();
     {%- if mcu == "esp32" %}
     let system = peripherals.DPORT.split();
@@ -49,8 +59,10 @@ fn init_heap() {
     let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks);
     let mut wdt1 = timer_group1.wdt;
 
-    {% if mcu == "esp32c3" -%}rtc.swd.disable();
-    {% endif -%}rtc.rwdt.disable();
+    {%- if mcu == "esp32c3" %}
+    rtc.swd.disable();
+    {% endif %}
+    rtc.rwdt.disable();
     wdt0.disable();
     wdt1.disable();
 
