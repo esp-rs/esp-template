a2554fb09e7be9343b41efc64923b2389122a729
Manuel Philipp <online@philipp-manuel.de>
2023-08-10 11:50:03 +0200

    added option to add support for the log crate (#100)
    * added option to add support for the log crate
    
    * added logging to ci matrix
    
    * switcth to init_logger_from_env and fixed typo in ci
    
    * fix format error

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,10 @@ extern crate alloc;
 use esp_backtrace as _;
 use esp_println::println;
 use hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, timer::TimerGroup, Rtc};
+{% if logging -%}
+use log::info;
+{% endif -%}
+
 {%- if alloc %}
 #[global_allocator]
 static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
@@ -69,6 +73,15 @@ fn main() -> ! {
     wdt1.disable();
     {% endif -%}
 
+    {% if logging -%}
+    // setup logger
+    // To change the log_level change the env section in .config/cargo.toml
+    // or remove it and set ESP_LOGLEVEL manually before running cargo run
+    // this requires a clean rebuild because of https://github.com/rust-lang/cargo/issues/10358
+    esp_println::logger::init_logger_from_env();
+    info!("Logger is setup");
+    {% endif -%}
+ 
     println!("Hello world!");
 
     loop {}
