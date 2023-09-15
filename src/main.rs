8c3a604f3d8d136e98c46bbcf3e64497bc35df95
Sergio Gasquez <sergio.gasquez@gmail.com>
2023-09-15 11:30:39 +0200

    feat: Add support for esp-wifi

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,28 @@ use core::mem::MaybeUninit;
 use esp_backtrace as _;
 use esp_println::println;
 use hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, Delay};
+
+{% if wifi -%}
+use embedded_svc::{
+    ipv4::Interface,
+    wifi::{AccessPointInfo, ClientConfiguration, Configuration, Wifi},
+};
+use esp_wifi::{
+    current_millis, initialize,
+    wifi::{utils::create_network_interface, WifiError, WifiMode},
+    wifi_interface::WifiStack,
+    EspWifiInitFor,
+};
+use hal::{systimer::SystemTimer, Rng};
+use smoltcp::iface::SocketStorage;
+{% endif -%}
 {% if logging -%}
 use log::info;
+{% endif -%}
+
+{% if wifi -%}
+const SSID: &str = env!("SSID");
+const PASSWORD: &str = env!("PASSWORD");
 {% endif -%}
 
 {%- if alloc %}
@@ -45,6 +65,74 @@ fn main() -> ! {
     {% endif -%}
 
     println!("Hello world!");
+
+
+    {% if wifi -%}
+    let timer = SystemTimer::new(peripherals.SYSTIMER).alarm0;
+    let init = initialize(
+        EspWifiInitFor::Wifi,
+        timer,
+        Rng::new(peripherals.RNG),
+        system.radio_clock_control,
+        &clocks,
+    )
+    .unwrap();
+    let (wifi, _) = peripherals.RADIO.split();
+    let mut socket_set_entries: [SocketStorage; 3] = Default::default();
+    let (iface, device, mut controller, sockets) =
+        create_network_interface(&init, wifi, WifiMode::Sta, &mut socket_set_entries).unwrap();
+    let wifi_stack = WifiStack::new(iface, device, sockets, current_millis);
+    let client_config = Configuration::Client(ClientConfiguration {
+        ssid: SSID.into(),
+        password: PASSWORD.into(),
+        ..Default::default()
+    });
+    let res = controller.set_configuration(&client_config);
+    println!("Wi-Fi set_configuration returned {:?}", res);
+
+    controller.start().unwrap();
+    println!("Is wifi started: {:?}", controller.is_started());
+
+    println!("Start Wifi Scan");
+    let res: Result<(heapless::Vec<AccessPointInfo, 10>, usize), WifiError> = controller.scan_n();
+    if let Ok((res, _count)) = res {
+        for ap in res {
+            println!("{:?}", ap);
+        }
+    }
+
+    println!("{:?}", controller.get_capabilities());
+    println!("Wi-Fi connect: {:?}", controller.connect());
+
+    // Wait to get connected
+    println!("Wait to get connected");
+    loop {
+        let res = controller.is_connected();
+        match res {
+            Ok(connected) => {
+                if connected {
+                    break;
+                }
+            }
+            Err(err) => {
+                println!("{:?}", err);
+                loop {}
+            }
+        }
+    }
+    println!("{:?}", controller.is_connected());
+
+    // Wait for getting an ip address
+    println!("Wait to get an ip address");
+    loop {
+        wifi_stack.work();
+
+        if wifi_stack.is_iface_up() {
+            println!("got ip {:?}", wifi_stack.get_ip_info());
+            break;
+        }
+    }
+    {% endif -%}
 
     loop {
         println!("Loop...");
