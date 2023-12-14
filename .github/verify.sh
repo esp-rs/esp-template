480b021d5e4518b7532b7610282788ef962a7765
Sergio Gasquez <sergio.gasquez@gmail.com>
2023-12-14 15:26:17 +0100

    ci: Remove wifi promt for H2

--- .github/verify.sh
+++ .github/verify.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "Verifying $1"
 # TODO: We might be able to check GITHUB_TOKEN and update the --path to make it work locally.
 
@@ -13,29 +15,31 @@ perform_checks() {
     cd ..
 }
 
-if [ "$1" = "esp32h2" ]; then
-    # ESP32-H22 doesn't have a WiFi core
-    wifi=false
-else
-    wifi=true
-fi
-
 complex_path="test-complex"
 simple_path="test-simple"
+simple_wifi_arg=""
+complex_wifi_arg=""
+
+# H2 has no wifi
+if [ "$1" != "esp32h2" ]; then
+    simple_wifi_arg="-d wifi=false"
+    complex_wifi_arg="-d wifi=true"
+fi
 
 # Complex template
 cargo generate \
     --path "/home/runner/work/esp-template/esp-template/esp-template-gh" \
     --silent --vcs=none --name=test-complex \
     -d advanced=true -d ci=false -d devcontainer=false -d wokwi=false \
-    -d alloc=true -d logging=true -d wifi=$wifi \
+    -d alloc=true -d logging=true $simple_wifi_arg \
     -d mcu=$1
+
 # Simple template
 cargo generate \
     --path "/home/runner/work/esp-template/esp-template/esp-template-gh" \
     --silent --vcs=none --name=test-simple \
     -d advanced=true -d ci=false -d devcontainer=false -d wokwi=false \
-    -d alloc=false -d logging=false -d wifi=false \
+    -d alloc=false -d logging=false $complex_wifi_arg \
     -d mcu=$1
 
 # Perform checks for complex and simple templates
