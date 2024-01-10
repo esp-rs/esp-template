a7d8a0180cd8fed861d49fd4bca0a710f81d2d22
Scott Mabin <scott@mabez.dev>
2024-01-10 16:59:07 +0000

    Split alloc and wifi into rhai snippets

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,34 @@
 #![no_std]
 #![no_main]
 
-{% if alloc -%}
-extern crate alloc;
-use core::mem::MaybeUninit;
-{% endif -%}
 use {{ mcu }}_hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, Delay};
 use esp_backtrace as _;
 use esp_println::println;
 
-{% if wifi -%}
-use esp_wifi::{initialize, EspWifiInitFor};
-
-{% if arch == "riscv" -%}
-use {{ mcu }}_hal::{systimer::SystemTimer, Rng};
-{% else -%}
-use {{ mcu }}_hal::{timer::TimerGroup, Rng};
-{% endif -%}
-{% endif -%}
-
 {% if alloc -%}
-#[global_allocator]
-static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
-
-fn init_heap() {
-    const HEAP_SIZE: usize = 32 * 1024;
-    static mut HEAP: MaybeUninit<[u8; HEAP_SIZE]> = MaybeUninit::uninit();
-
-    unsafe {
-        ALLOCATOR.init(HEAP.as_mut_ptr() as *mut u8, HEAP_SIZE);
-    }
-}
+{{ alloc_snippet }}
 {% endif -%}
+
 #[entry]
 fn main() -> ! {
-    {%- if alloc %}
-    init_heap();
-    {%- endif %}
     let peripherals = Peripherals::take();
     let system = peripherals.SYSTEM.split();
 
     let clocks = ClockControl::max(system.clock_control).freeze();
     let mut delay = Delay::new(&clocks);
 
+    {%- if alloc %}
+    init_heap();
+    {%- endif %}
+
     esp_println::logger::init_logger_from_env();
-    log::info!("Hello world!");
+
     {% if wifi -%}
-    {% if arch == "riscv" -%}
-    let timer = SystemTimer::new(peripherals.SYSTIMER).alarm0;
-    {% else -%}
-    let timer = TimerGroup::new(peripherals.TIMG1, &clocks).timer0;
+    {{ esp_wifi_snippet }}
     {% endif -%}
-    let _init = initialize(
-        EspWifiInitFor::Wifi,
-        timer,
-        Rng::new(peripherals.RNG),
-        system.radio_clock_control,
-        &clocks,
-    )
-    .unwrap();
-    {% endif -%}
+    
     loop {
-        println!("Loop...");
+        log::info!("Hello world!");
         delay.delay_ms(500u32);
     }
 }
