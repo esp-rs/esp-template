e4260f9cac72b2dd1d46c9f67b69f6f836c44ddd
Scott Mabin <scott@mabez.dev>
2024-01-10 14:36:24 +0000

    Default to logging

--- src/main.rs
+++ src/main.rs
@@ -43,15 +43,8 @@ fn main() -> ! {
     let clocks = ClockControl::max(system.clock_control).freeze();
     let mut delay = Delay::new(&clocks);
 
-    {% if logging -%}
-    // setup logger
-    // To change the log_level change the env section in .cargo/config.toml
-    // or remove it and set ESP_LOGLEVEL manually before running cargo run
-    // this requires a clean rebuild because of https://github.com/rust-lang/cargo/issues/10358
     esp_println::logger::init_logger_from_env();
-    log::info!("Logger is setup");
-    {% endif -%}
-    println!("Hello world!");
+    log::info!("Hello world!");
     {% if wifi -%}
     {% if arch == "riscv" -%}
     let timer = SystemTimer::new(peripherals.SYSTIMER).alarm0;
